3730675b25333ce4b5479f27955cfe4ab2d88bfa
kocurvik <kocurvik@gmail.com>
2020-12-31 01:51:18 +0100

    fixed crash after rotation bug

--- app/src/main/java/com/kocur/tabapp/DatePickerFragment.java
+++ app/src/main/java/com/kocur/tabapp/DatePickerFragment.java
@@ -2,11 +2,15 @@ package com.kocur.tabapp;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.util.Calendar;
@@ -34,6 +38,20 @@ public class DatePickerFragment extends DialogFragment
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
+    public void onConfigurationChanged(Configuration configuration) {
+        super.onConfigurationChanged(configuration);
+        Log.d("Orientation", "Changed orientation recreating DatePickerFragment!");
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.setEditText(editText);
+        if (tabAnalytics != null)
+            newFragment.setTabAnalytics(tabAnalytics);
+        if (tabLog != null)
+            newFragment.setTabLog(tabLog);
+
+        newFragment.show(getFragmentManager(), "datePicker");
+        dismiss();
+    }
+
     public void setEditText(EditText editText){
         this.editText = editText;
     }
@@ -47,6 +65,13 @@ public class DatePickerFragment extends DialogFragment
      */
     public void onDateSet(DatePicker view, int year, int month, int day) {
         View rootView = view.getRootView();
+        if (editText == null) {
+            Toast toast = Toast.makeText(getContext(), "Something went wrong!", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+            dismiss();
+            return;
+        }
         String oldtext = editText.getText().toString();
         editText.setText(String.format(Locale.US,"%02d",day) + "/" + String.format(Locale.US, "%02d",month+1) + "/" + String.format(Locale.US, "%04d",year));
         if (tabLog != null){
